b3ef748c881553a35cadde85979f18fe93104f40
mikael <13247629622@163.com>
2022-12-03 12:07:36 +0800

    add mysql json to pb string

--- core/core.go
+++ core/core.go
@@ -30,7 +30,7 @@ const (
 // Do not rely on the structure of the Generated schema to provide any context about
 // the protobuf types. The schema reflects the layout of a protobuf file and should be used
 // to pipe the output of the `Schema.String()` to a file.
-func GenerateSchema(db *sql.DB, table string, ignoreTables []string, serviceName,goPkg, pkg string) (*Schema, error) {
+func GenerateSchema(db *sql.DB, table string, ignoreTables []string, serviceName, goPkg, pkg string) (*Schema, error) {
 	s := &Schema{}
 
 	dbs, err := dbSchema(db)
@@ -84,7 +84,7 @@ func typesFromColumns(s *Schema, cols []Column, ignoreTables []string) error {
 
 		msg, ok := messageMap[messageName]
 		if !ok {
-			messageMap[messageName] = &Message{Name: messageName,Comment: c.TableComment}
+			messageMap[messageName] = &Message{Name: messageName, Comment: c.TableComment}
 			msg = messageMap[messageName]
 		}
 
@@ -111,7 +111,7 @@ func dbSchema(db *sql.DB) (string, error) {
 
 func dbColumns(db *sql.DB, schema, table string) ([]Column, error) {
 
-	tableArr:= strings.Split(table,",")
+	tableArr := strings.Split(table, ",")
 
 	q := "SELECT c.TABLE_NAME, c.COLUMN_NAME, c.IS_NULLABLE, c.DATA_TYPE, " +
 		"c.CHARACTER_MAXIMUM_LENGTH, c.NUMERIC_PRECISION, c.NUMERIC_SCALE, c.COLUMN_TYPE ,c.COLUMN_COMMENT,t.TABLE_COMMENT " +
@@ -119,10 +119,10 @@ func dbColumns(db *sql.DB, schema, table string) ([]Column, error) {
 		" WHERE c.TABLE_SCHEMA = ?"
 
 	if table != "" && table != "*" {
-		q +=  " AND c.TABLE_NAME IN('" + strings.TrimRight(strings.Join(tableArr,"' ,'"),",") + "')"
+		q += " AND c.TABLE_NAME IN('" + strings.TrimRight(strings.Join(tableArr, "' ,'"), ",") + "')"
 	}
 
-	q +=  " ORDER BY c.TABLE_NAME, c.ORDINAL_POSITION"
+	q += " ORDER BY c.TABLE_NAME, c.ORDINAL_POSITION"
 
 	rows, err := db.Query(q, schema)
 	defer rows.Close()
@@ -135,12 +135,12 @@ func dbColumns(db *sql.DB, schema, table string) ([]Column, error) {
 	for rows.Next() {
 		cs := Column{}
 		err := rows.Scan(&cs.TableName, &cs.ColumnName, &cs.IsNullable, &cs.DataType,
-			&cs.CharacterMaximumLength, &cs.NumericPrecision, &cs.NumericScale, &cs.ColumnType,&cs.ColumnComment,&cs.TableComment)
+			&cs.CharacterMaximumLength, &cs.NumericPrecision, &cs.NumericScale, &cs.ColumnType, &cs.ColumnComment, &cs.TableComment)
 		if err != nil {
 			log.Fatal(err)
 		}
 
-		if cs.TableComment == ""{
+		if cs.TableComment == "" {
 			cs.TableComment = stringx.From(cs.TableName).ToCamelWithStartLower()
 		}
 
@@ -155,13 +155,13 @@ func dbColumns(db *sql.DB, schema, table string) ([]Column, error) {
 
 // Schema is a representation of a protobuf schema.
 type Schema struct {
-	Syntax    string
+	Syntax      string
 	ServiceName string
-	GoPackage string
-	Package   string
-	Imports   sort.StringSlice
-	Messages  MessageCollection
-	Enums     EnumCollection
+	GoPackage   string
+	Package     string
+	Imports     sort.StringSlice
+	Messages    MessageCollection
+	Enums       EnumCollection
 }
 
 // MessageCollection represents a sortable collection of messages.
@@ -225,7 +225,7 @@ func (s *Schema) String() string {
 	buf.WriteString("// ------------------------------------ \n\n")
 
 	for _, m := range s.Messages {
-		buf.WriteString("//--------------------------------" + m.Comment+"--------------------------------")
+		buf.WriteString("//--------------------------------" + m.Comment + "--------------------------------")
 		buf.WriteString("\n")
 		m.GenDefaultMessage(buf)
 		m.GenRpcAddReqRespMessage(buf)
@@ -237,7 +237,7 @@ func (s *Schema) String() string {
 
 	buf.WriteString("\n")
 
-	if len(s.Enums) > 0{
+	if len(s.Enums) > 0 {
 		buf.WriteString("// ------------------------------------ \n")
 		buf.WriteString("// Enums\n")
 		buf.WriteString("// ------------------------------------ \n\n")
@@ -254,7 +254,7 @@ func (s *Schema) String() string {
 
 	funcTpl := "service " + s.ServiceName + "{ \n\n"
 	for _, m := range s.Messages {
-		funcTpl+= "\t //-----------------------" + m.Comment+"----------------------- \n"
+		funcTpl += "\t //-----------------------" + m.Comment + "----------------------- \n"
 		funcTpl += "\t rpc Add" + m.Name + "(Add" + m.Name + "Req) returns (Add" + m.Name + "Resp); \n"
 		funcTpl += "\t rpc Update" + m.Name + "(Update" + m.Name + "Req) returns (Update" + m.Name + "Resp); \n"
 		funcTpl += "\t rpc Del" + m.Name + "(Del" + m.Name + "Req) returns (Del" + m.Name + "Resp); \n"
@@ -269,9 +269,9 @@ func (s *Schema) String() string {
 
 // Enum represents a protocol buffer enumerated type.
 type Enum struct {
-	Name   string
+	Name    string
 	Comment string
-	Fields []EnumField
+	Fields  []EnumField
 }
 
 // String returns a string representation of an Enum.
@@ -335,7 +335,7 @@ func (ef EnumField) Tag() int {
 }
 
 // newEnumFromStrings creates an enum from a name and a slice of strings that represent the names of each field.
-func newEnumFromStrings(name ,comment string, ss []string) (*Enum, error) {
+func newEnumFromStrings(name, comment string, ss []string) (*Enum, error) {
 	enum := &Enum{}
 	enum.Name = name
 	enum.Comment = comment
@@ -361,7 +361,7 @@ type Message struct {
 	Fields  []MessageField
 }
 
-//gen default message
+// gen default message
 func (m Message) GenDefaultMessage(buf *bytes.Buffer) {
 	mOrginName := m.Name
 	mOrginFields := m.Fields
@@ -375,7 +375,7 @@ func (m Message) GenDefaultMessage(buf *bytes.Buffer) {
 		filedTag++
 		field.tag = filedTag
 		field.Name = stringx.From(field.Name).ToCamelWithStartLower()
-		if field.Comment == ""{
+		if field.Comment == "" {
 			field.Comment = field.Name
 		}
 		curFields = append(curFields, field)
@@ -388,7 +388,7 @@ func (m Message) GenDefaultMessage(buf *bytes.Buffer) {
 	m.Fields = mOrginFields
 }
 
-//gen add req message
+// gen add req message
 func (m Message) GenRpcAddReqRespMessage(buf *bytes.Buffer) {
 	mOrginName := m.Name
 	mOrginFields := m.Fields
@@ -404,7 +404,7 @@ func (m Message) GenRpcAddReqRespMessage(buf *bytes.Buffer) {
 		filedTag++
 		field.tag = filedTag
 		field.Name = stringx.From(field.Name).ToCamelWithStartLower()
-		if field.Comment == ""{
+		if field.Comment == "" {
 			field.Comment = field.Name
 		}
 		curFields = append(curFields, field)
@@ -427,7 +427,7 @@ func (m Message) GenRpcAddReqRespMessage(buf *bytes.Buffer) {
 
 }
 
-//gen add resp message
+// gen add resp message
 func (m Message) GenRpcUpdateReqMessage(buf *bytes.Buffer) {
 	mOrginName := m.Name
 	mOrginFields := m.Fields
@@ -442,7 +442,7 @@ func (m Message) GenRpcUpdateReqMessage(buf *bytes.Buffer) {
 		filedTag++
 		field.tag = filedTag
 		field.Name = stringx.From(field.Name).ToCamelWithStartLower()
-		if field.Comment == ""{
+		if field.Comment == "" {
 			field.Comment = field.Name
 		}
 		curFields = append(curFields, field)
@@ -464,14 +464,14 @@ func (m Message) GenRpcUpdateReqMessage(buf *bytes.Buffer) {
 	m.Fields = mOrginFields
 }
 
-//gen add resp message
+// gen add resp message
 func (m Message) GenRpcDelReqMessage(buf *bytes.Buffer) {
 	mOrginName := m.Name
 	mOrginFields := m.Fields
 
 	m.Name = "Del" + mOrginName + "Req"
 	m.Fields = []MessageField{
-		{Name: "id", Typ: "int64", tag: 1,Comment: "id"},
+		{Name: "id", Typ: "int64", tag: 1, Comment: "id"},
 	}
 	buf.WriteString(fmt.Sprintf("%s\n", m))
 
@@ -489,14 +489,14 @@ func (m Message) GenRpcDelReqMessage(buf *bytes.Buffer) {
 	m.Fields = mOrginFields
 }
 
-//gen add resp message
+// gen add resp message
 func (m Message) GenRpcGetByIdReqMessage(buf *bytes.Buffer) {
 	mOrginName := m.Name
 	mOrginFields := m.Fields
 
 	m.Name = "Get" + mOrginName + "ByIdReq"
 	m.Fields = []MessageField{
-		{Name: "id", Typ: "int64", tag: 1,Comment: "id"},
+		{Name: "id", Typ: "int64", tag: 1, Comment: "id"},
 	}
 	buf.WriteString(fmt.Sprintf("%s\n", m))
 
@@ -508,7 +508,7 @@ func (m Message) GenRpcGetByIdReqMessage(buf *bytes.Buffer) {
 	firstWord := strings.ToLower(string(m.Name[0]))
 	m.Name = "Get" + mOrginName + "ByIdResp"
 	m.Fields = []MessageField{
-		{Typ: mOrginName, Name: stringx.From(firstWord + mOrginName[1:]).ToCamelWithStartLower(), tag: 1,Comment: stringx.From(firstWord + mOrginName[1:]).ToCamelWithStartLower()},
+		{Typ: mOrginName, Name: stringx.From(firstWord + mOrginName[1:]).ToCamelWithStartLower(), tag: 1, Comment: stringx.From(firstWord + mOrginName[1:]).ToCamelWithStartLower()},
 	}
 	buf.WriteString(fmt.Sprintf("%s\n", m))
 
@@ -517,17 +517,17 @@ func (m Message) GenRpcGetByIdReqMessage(buf *bytes.Buffer) {
 	m.Fields = mOrginFields
 }
 
-//gen add resp message
+// gen add resp message
 func (m Message) GenRpcSearchReqMessage(buf *bytes.Buffer) {
 	mOrginName := m.Name
 	mOrginFields := m.Fields
 
 	m.Name = "Search" + mOrginName + "Req"
 	curFields := []MessageField{
-		{Typ: "int64",Name: "page",tag: 1,Comment: "page"},
-		{Typ: "int64",Name: "pageSize",tag: 2,Comment: "pageSize"},
+		{Typ: "int64", Name: "page", tag: 1, Comment: "page"},
+		{Typ: "int64", Name: "pageSize", tag: 2, Comment: "pageSize"},
 	}
-	var filedTag  = len(curFields)
+	var filedTag = len(curFields)
 	for _, field := range m.Fields {
 		if isInSlice([]string{"version", "del_state", "delete_time"}, field.Name) {
 			continue
@@ -535,7 +535,7 @@ func (m Message) GenRpcSearchReqMessage(buf *bytes.Buffer) {
 		filedTag++
 		field.tag = filedTag
 		field.Name = stringx.From(field.Name).ToCamelWithStartLower()
-		if field.Comment == ""{
+		if field.Comment == "" {
 			field.Comment = field.Name
 		}
 		curFields = append(curFields, field)
@@ -551,7 +551,7 @@ func (m Message) GenRpcSearchReqMessage(buf *bytes.Buffer) {
 	firstWord := strings.ToLower(string(m.Name[0]))
 	m.Name = "Search" + mOrginName + "Resp"
 	m.Fields = []MessageField{
-		{Typ: "repeated " + mOrginName, Name: stringx.From(firstWord + mOrginName[1:]).ToCamelWithStartLower(), tag: 1,Comment: stringx.From(firstWord + mOrginName[1:]).ToCamelWithStartLower()},
+		{Typ: "repeated " + mOrginName, Name: stringx.From(firstWord + mOrginName[1:]).ToCamelWithStartLower(), tag: 1, Comment: stringx.From(firstWord + mOrginName[1:]).ToCamelWithStartLower()},
 	}
 	buf.WriteString(fmt.Sprintf("%s\n", m))
 
@@ -566,7 +566,7 @@ func (m Message) String() string {
 
 	buf.WriteString(fmt.Sprintf("message %s {\n", m.Name))
 	for _, f := range m.Fields {
-		buf.WriteString(fmt.Sprintf("%s%s; //%s\n", indent, f,f.Comment))
+		buf.WriteString(fmt.Sprintf("%s%s; //%s\n", indent, f, f.Comment))
 	}
 	buf.WriteString("}\n")
 
@@ -588,15 +588,15 @@ func (m *Message) AppendField(mf MessageField) error {
 
 // MessageField represents the field of a message.
 type MessageField struct {
-	Typ  string
-	Name string
-	tag  int
+	Typ     string
+	Name    string
+	tag     int
 	Comment string
 }
 
 // NewMessageField creates a new message field.
-func NewMessageField(typ, name string, tag int,comment string) MessageField {
-	return MessageField{typ, name, tag,comment}
+func NewMessageField(typ, name string, tag int, comment string) MessageField {
+	return MessageField{typ, name, tag, comment}
 }
 
 // Tag returns the unique numbered tag of the message field.
@@ -622,10 +622,11 @@ type Column struct {
 	ColumnType             string
 	ColumnComment          string
 }
+
 // Table represents a database table.
 type Table struct {
-	TableName              string
-	ColumnName             string
+	TableName  string
+	ColumnName string
 }
 
 // parseColumn parses a column and inserts the relevant fields in the Message. If an enumerated type is encountered, an Enum will
@@ -646,7 +647,7 @@ func parseColumn(s *Schema, msg *Message, col Column) error {
 		})
 
 		enumName := inflect.Singularize(snaker.SnakeToCamel(col.TableName)) + snaker.SnakeToCamel(col.ColumnName)
-		enum, err := newEnumFromStrings(enumName,col.ColumnComment, enums)
+		enum, err := newEnumFromStrings(enumName, col.ColumnComment, enums)
 		if nil != err {
 			return err
 		}
@@ -659,19 +660,21 @@ func parseColumn(s *Schema, msg *Message, col Column) error {
 	case "date", "time", "datetime", "timestamp":
 		//s.AppendImport("google/protobuf/timestamp.proto")
 		fieldType = "int64"
-	case  "bool", "bit":
+	case "bool", "bit":
 		fieldType = "bool"
-	case "tinyint","smallint", "int", "mediumint", "bigint":
+	case "tinyint", "smallint", "int", "mediumint", "bigint":
 		fieldType = "int64"
 	case "float", "decimal", "double":
 		fieldType = "double"
+	case "json":
+		fieldType = "string"
 	}
 
 	if "" == fieldType {
 		return fmt.Errorf("no compatible protobuf type found for `%s`. column: `%s`.`%s`", col.DataType, col.TableName, col.ColumnName)
 	}
 
-	field := NewMessageField(fieldType, col.ColumnName, len(msg.Fields)+1,col.ColumnComment)
+	field := NewMessageField(fieldType, col.ColumnName, len(msg.Fields)+1, col.ColumnComment)
 
 	err := msg.AppendField(field)
 	if nil != err {
